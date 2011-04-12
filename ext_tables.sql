8f5e7db53c9b9fcf1299ee7674eadee975edc434
Michael Knoll <mimi@kaktusteam.de>
2011-04-12 23:10:04 +0200

    ADD: Added SQL definition for category table

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,3 +1,37 @@
+CREATE TABLE tx_yag_domain_model_category (
+    uid int(11) NOT NULL auto_increment,
+    pid int(11) DEFAULT '0' NOT NULL,
+    
+    name  VARCHAR(50) DEFAULT ''  NOT NULL,
+    description text,
+    lft   INT(12) DEFAULT '0' NOT NULL,
+    rgt   INT(12) DEFAULT '0' NOT NULL,
+
+    tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+
+    t3ver_oid int(11) DEFAULT '0' NOT NULL,
+    t3ver_id int(11) DEFAULT '0' NOT NULL,
+    t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+    t3ver_label varchar(30) DEFAULT '' NOT NULL,
+    t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_count int(11) DEFAULT '0' NOT NULL,
+    t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+    t3_origuid int(11) DEFAULT '0' NOT NULL,
+
+    sys_language_uid int(11) DEFAULT '0' NOT NULL,
+    l18n_parent int(11) DEFAULT '0' NOT NULL,
+    l18n_diffsource mediumblob NOT NULL,
+
+    PRIMARY KEY (uid),
+    KEY parent (pid),
+    key lft (lft),
+    key rgt (rgt)
+);
+
 CREATE TABLE tx_yag_domain_model_album (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
