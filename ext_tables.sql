c55a693502ba13405ea753b177a01d7bf9ce8281
tottt <dodosch@gmail.com>
2011-10-10 12:50:43 +0300

    Added "author" field

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,6 +6,7 @@ CREATE TABLE tx_yag_domain_model_album (
 	
     hide tinyint(4) unsigned DEFAULT '0' NOT NULL,
 	name tinytext,
+	author tinytext,
 	description text,
 	items int(11) unsigned DEFAULT '0' NOT NULL,
 	galleries int(11) unsigned DEFAULT '0' NOT NULL,
